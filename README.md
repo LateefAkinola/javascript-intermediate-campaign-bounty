ae8e95fc67d71b494996fb969bcc2707ebf34ed5
Lateef Olanrewaju Akinola <akinolalateefolanrewaju@gmail.com>
2023-09-23 00:16:20 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,155 @@
 # javascript-intermediate-campaign-bounty
+
+# Interactive JavaScript Code Explanation
+
+In this Markdown file, we'll provide a step-by-step and detailed explanation of the provided JavaScript code for a simple online bookstore application. The code allows users to view books, add them to a cart, check out, and perform various actions. We'll specifically focus on how classes, switch statements, and try-catch-finally statements are used in this code.
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Book Class](#book-class)
+- [Initializing Books](#initializing-books)
+- [Displaying Books](#displaying-books)
+- [User Actions](#user-actions)
+  - [Using Switch Statements](#using-switch-statements)
+- [Toggling Book Details](#toggling-book-details)
+- [Adding Books to the Cart](#adding-books-to-the-cart)
+  - [Using Try-Catch-Finally Statements](#using-try-catch-finally-statements)
+- [Displaying the Cart](#displaying-the-cart)
+- [Checkout Process](#checkout-process)
+- [Clearing the Cart](#clearing-the-cart)
+- [Toggling Cart Visibility](#toggling-cart-visibility)
+- [Adding a Book to the Store](#adding-a-book-to-the-store)
+- [Conclusion](#conclusion)
+
+---
+
+## Introduction
+
+This JavaScript code represents a simple online bookstore application. Users can view books, add them to a cart, check out, and more. We'll explain each part of the code in detail, with a focus on how classes, switch statements, and try-catch-finally statements are utilized.
+
+---
+
+## Book Class
+
+### `class Book`
+
+- This class defines the structure of a book object.
+- It has properties such as `title`, `author`, `price`, and `stockQuantity`.
+- A `constructor` initializes these properties when a book object is created.
+
+---
+
+## Initializing Books
+
+### `const books`
+
+- An array of `Book` objects is initialized, representing the available books in the store.
+- Each book object is created with a title, author, price, and stock quantity.
+
+---
+
+## Displaying Books
+
+### `function displayBooks()`
+
+- This function populates the webpage with book information.
+- It creates HTML elements for each book and displays details like title, author, price, and stock quantity.
+- Users can click a button to toggle book details visibility.
+- The "Add to Cart" button allows adding books to the cart, but it checks if the book is in stock.
+
+---
+
+## User Actions
+
+### `function handleUserAction(action, index)`
+
+- This function handles user actions based on the provided action type and book index.
+- Actions include viewing details, adding to the cart, checking out, clearing the cart, and toggling cart visibility.
+
+#### Using Switch Statements
+
+- Inside `handleUserAction`, switch statements are used to determine the action type and execute the corresponding code block.
+- This approach keeps the code organized and makes it easy to add new actions in the future.
+
+---
+
+## Toggling Book Details
+
+### `function toggleBookDetails(index)`
+
+- Toggles the visibility of book details (author, price, and stock quantity) when the user clicks the "View Book Details" button.
+
+---
+
+## Adding Books to the Cart
+
+### `function addToCart(index)`
+
+- Adds a selected book to the cart if it's in stock.
+- Decreases the book's stock quantity and updates the UI.
+- Throws an error if the book is out of stock.
+
+#### Using Try-Catch-Finally Statements
+
+- Try-catch-finally statements are used to handle potential errors when adding books to the cart.
+- The try block attempts to add the book to the cart, and if successful, it updates the UI.
+- If an error occurs (e.g., out of stock), the catch block handles the error and displays an error message to the user.
+- The finally block is used for any clean-up or final actions if needed.
+
+---
+
+## Displaying the Cart
+
+### `function displayCart()`
+
+- Displays the contents of the cart on the webpage.
+- Lists the titles and prices of books in the cart.
+- Calculates and displays the total price of items in the cart.
+
+---
+
+## Checkout Process
+
+### `function checkout()`
+
+- Allows the user to proceed with the checkout process.
+- Clears the cart after confirming the purchase.
+
+---
+
+## Clearing the Cart
+
+### `function clearCart()`
+
+- Clears the cart and returns books to the shelves.
+- Increases the stock quantity of each book in the cart.
+- Updates the displayed book list and cart display.
+
+---
+
+## Toggling Cart Visibility
+
+### `function toggleCart()`
+
+- Toggles the visibility of the cart when the user clicks the "View Cart" button.
+- Updates the cart button text accordingly.
+
+---
+
+## Adding a Book to the Store
+
+### `function addBookToStore()`
+
+- Prompts the user to enter details for a new book (title, author, price, stock quantity).
+- Validates the input to ensure it's valid and then creates a new `Book` object.
+- Adds the new book to the store's inventory (the `books` array) and updates the UI.
+
+---
+
+## Conclusion
+
+This JavaScript code provides the foundation for a basic online bookstore application. It allows users to interact with books, add them to a cart, check out, and even manage the store's inventory. Understanding each part of the code is essential for maintaining and extending the functionality of the application.
+
+For a more interactive experience, consider embedding this markdown content into an interactive markdown editor or a web platform that supports interactive documentation. This will allow users to collapse/expand sections, execute code snippets, and interact with the explanations for a richer learning experience.
+
+Feel free to use, modify, or extend this code for your own projects.
--- src/index.js
+++ src/index.js
@@ -1,74 +1,86 @@
+// Define a class called Book to create book objects
 class Book {
     constructor(title, author, price, stockQuantity) {
-        this.title = title;
-        this.author = author;
-        this.price = price;
-        this.stockQuantity = stockQuantity;
+        // Constructor to initialize book properties
+        this.title = title;           // Title of the book
+        this.author = author;         // Author of the book
+        this.price = price;           // Price of the book
+        this.stockQuantity = stockQuantity; // Stock quantity of the book
     }
 }
 
+// Create an array called 'books' to store instances of Book class
 const books = [
-    new Book("Book 1", "Author 1", 15.99, 10),
-    new Book("Book 2", "Author 2", 12.99, 5),
-    new Book("Book 3", "Author 3", 9.99, 8),
+    new Book("A Tale of Two Cities", "Charles Dickens", 55.99, 108),     // Example book 1
+    new Book("The Hobbit", "J. R. R. Tolkien", 120.99, 90),            // Example book 2
+    new Book("The Da Vinci Code", "Dan Brown", 109.99, 50),            // Example book 3
 ];
 
-const cart = [];
-let cartHidden = true; // Variable to track cart visibility
+// Create an empty array called 'cart' to store books added to the cart
+let cart = [];
 
-// Function to display books
+// Create a variable 'cartHidden' to track the visibility of the cart (initially hidden)
+let cartHidden = true;
+
+// Function to display books on the webpage
 function displayBooks() {
+    // Get the HTML element with the id 'books'
     const booksContainer = document.getElementById("books");
+    
+    // Clear the existing content inside the 'books' container
     booksContainer.innerHTML = "";
 
+    // Iterate over the 'books' array and create HTML elements for each book
     books.forEach((book, index) => {
-        const bookElement = document.createElement("div");
-        bookElement.classList.add("book");
-        bookElement.setAttribute("id", `book-${index}`);
+        const bookElement = document.createElement("div"); // Create a <div> for the book
+        bookElement.classList.add("book"); // Add a CSS class 'book' to the <div>
+        bookElement.setAttribute("id", `book-${index}`); // Set a unique id for each book <div>
         bookElement.innerHTML = `
             <div class="book-details">
-                <p><strong>Title:</strong> ${book.title}</p>
-                <p class="author hidden"><strong>Author:</strong> ${book.author}</p>
-                <p class="price hidden"><strong>Price:</strong> $${book.price}</p>
-                <p class="stock hidden"><strong>Stock Quantity:</strong> ${book.stockQuantity}</p>
+                <p><strong>Title:</strong> ${book.title}</p>     <!-- Display book title -->
+                <p class="author hidden"><strong>Author:</strong> ${book.author}</p>  <!-- Display book author (initially hidden) -->
+                <p class="price hidden"><strong>Price:</strong> $${book.price}</p>    <!-- Display book price (initially hidden) -->
+                <p class="stock hidden"><strong>Stock Quantity:</strong> ${book.stockQuantity}</p> <!-- Display stock quantity (initially hidden) -->
             </div>
-            <button onclick="toggleBookDetails(${index})">View Book Details</button>
-            <button onclick="handleUserAction('add', ${index})" ${book.stockQuantity === 0 ? 'disabled' : ''}>Add to Cart</button>
+            <button onclick="toggleBookDetails(${index})">View Book Details</button> <!-- Button to show/hide book details -->
+            <button onclick="handleUserAction('add', ${index})" ${book.stockQuantity === 0 ? 'disabled' : ''}>Add to Cart</button> <!-- Button to add the book to the cart -->
         `;
+
+        // Append the book <div> to the 'books' container
         booksContainer.appendChild(bookElement);
     });
 
     // Check if the cart should be hidden initially
     if (cartHidden) {
         const cartContainer = document.getElementById("cart");
-        cartContainer.classList.add("hidden");
+        cartContainer.classList.add("hidden"); // Add 'hidden' class to hide the cart
     }
 }
 
-// Function to handle user actions
+// Function to handle user actions based on the provided action type and book index
 function handleUserAction(action, index) {
     switch (action) {
         case 'view':
-            toggleBookDetails(index);
+            toggleBookDetails(index); // Toggle book details visibility
             break;
         case 'add':
-            addToCart(index);
+            addToCart(index); // Add the selected book to the cart
             break;
         case 'checkout':
-            checkout();
+            checkout(); // Proceed to checkout (empty the cart)
             break;
         case 'clearCart':
-            clearCart();
-            break;        
+            clearCart(); // Clear the cart (return books to the shelves)
+            break;
         case 'toggleCart':
-            toggleCart();
+            toggleCart(); // Toggle cart visibility
             break;
         default:
-            alert("Invalid action.");
+            alert("Invalid action."); // Display an alert for invalid actions
     }
 }
 
-// Function to toggle book details visibility
+// Function to toggle the visibility of book details based on the index
 function toggleBookDetails(index) {
     const bookDetails = document.querySelector(`#book-${index} .book-details`);
     const author = document.querySelector(`#book-${index} .author`);
@@ -76,54 +88,57 @@ function toggleBookDetails(index) {
     const stock = document.querySelector(`#book-${index} .stock`);
     const button = document.querySelector(`#book-${index} button`);
 
-    author.classList.toggle("hidden");
-    price.classList.toggle("hidden");
-    stock.classList.toggle("hidden");
+    author.classList.toggle("hidden"); // Toggle author visibility
+    price.classList.toggle("hidden");   // Toggle price visibility
+    stock.classList.toggle("hidden");   // Toggle stock quantity visibility
 
     if (author.classList.contains("hidden")) {
-        button.textContent = "View Book Details";
+        button.textContent = "View Book Details"; // Update button text when details are hidden
     } else {
-        button.textContent = "Hide Book Details";
+        button.textContent = "Hide Book Details"; // Update button text when details are shown
     }
 }
 
 // Function to add a book to the cart
 function addToCart(index) {
-    const book = books[index];
+    const book = books[index]; // Get the selected book from the 'books' array
     try {
         if (book.stockQuantity > 0) {
-            cart.push(book);
-            book.stockQuantity--;
-            displayBooks();
-            displayCart();
+            cart.push(book); // Add the book to the cart
+            book.stockQuantity--; // Decrease the stock quantity
+            displayBooks(); // Update the displayed book list
+            displayCart(); // Update the cart display
+            alert('Success'); // Show a success message
         } else {
-            throw new Error("Sorry, this book is out of stock.");
+            throw new Error("Sorry, this book is out of stock."); // Throw an error if the book is out of stock
         }
     } catch (error) {
-        alert(error.message);
+        alert(error.message); // Display an error message
     } finally {
-        // Clean up or perform final actions if needed
+        displayBooks(); // Ensure that book display is updated
     }
 }
 
-
-// Function to display the cart
+// Function to display the cart on the webpage
 function displayCart() {
-    const cartContainer = document.getElementById("cart");
-    cartContainer.innerHTML = "";
+    const cartContainer = document.getElementById("cart"); // Get the cart container element
+    cartContainer.innerHTML = ""; // Clear the existing cart content
 
-    let totalPrice = 0;
+    let totalPrice = 0; // Initialize the total price of items in the cart
 
     cart.forEach((book) => {
+        // Create a <div> for each book in the cart and display its title and price
         const cartItem = document.createElement("div");
         cartItem.innerHTML = `<strong>${book.title}</strong> - $${book.price}`;
-        cartContainer.appendChild(cartItem);
-        totalPrice += book.price;
+        cartContainer.appendChild(cartItem); // Append the cart item to the cart container
+
+        totalPrice += book.price; // Add the book's price to the total price
     });
 
+    // Create an element to display the total price of items in the cart
     const totalPriceElement = document.createElement("div");
     totalPriceElement.innerHTML = `<strong>Total Price: $${totalPrice.toFixed(2)}</strong>`;
-    cartContainer.appendChild(totalPriceElement);
+    cartContainer.appendChild(totalPriceElement); // Append the total price element to the cart container
 }
 
 // Function to handle the checkout process
@@ -133,23 +148,38 @@ function checkout() {
     } else {
         alert("Thank you for your purchase! Your books will be shipped to you soon.");
         cart.length = 0; // Clear the cart after checkout
-        displayCart();
+        displayCart(); // Update the cart display
     }
 }
 
-// Function to clear the cart
+// Function to clear the cart and return books to the shelves
 function clearCart() {
-    cart.length = 0; // Clear the cart
-    displayCart();
+    if (cart.length === 0) {
+        alert("Your cart is already empty.");
+    } else {
+        cart.forEach((book) => {
+            // Increase the stock quantity of each book in the cart
+            const index = books.findIndex((storeBook) => storeBook.title === book.title);
+            if (index !== -1) {
+                books[index].stockQuantity += 1;
+            }
+        });
+
+        cart.length = 0; // Clear the cart
+        displayBooks(); // Update the displayed book list to reflect changes in stock quantity
+        displayCart(); // Clear the cart display
+        alert("Cart cleared, and books returned to the shelves.");
+    }
 }
 
-// Function to toggle the cart visibility
+// Function to toggle the visibility of the cart
 function toggleCart() {
-    const cartContainer = document.getElementById("cart");
-    cartContainer.classList.toggle("hidden");
-    cartHidden = !cartHidden; // Update cartHidden variable
-    const cartButton = document.getElementById("cartButton");
-    cartButton.textContent = cartHidden ? 'View Cart' : 'Hide Cart';
+    const cartContainer = document.getElementById("cart"); // Get the cart container element
+    cartContainer.classList.toggle("hidden"); // Toggle the 'hidden' class to show/hide the cart
+    cartHidden = !cartHidden; // Update the cartHidden variable
+
+    const cartButton = document.getElementById("cartButton"); // Get the cart toggle button
+    cartButton.textContent = cartHidden ? 'View Cart' : 'Hide Cart'; // Update the button text
 }
 
 // Function to add a book to the store
@@ -166,21 +196,16 @@ function addBookToStore() {
             throw new Error("Invalid input. Please enter valid book details.");
         }
 
-        // Create a new book object and add it to the store
+        // Create a new book object and add it to the 'books' array
         const newBook = new Book(title, author, price, stockQuantity);
         books.push(newBook);
-        displayBooks();
         alert("Book added to the store successfully.");
     } catch (error) {
-        alert(error.message);
+        alert(error.message); // Display an error message if input is invalid
     } finally {
-        // Clean up or perform final actions if needed
+        displayBooks(); // Update the displayed book list
     }
 }
 
-// Add an event listener for the "Add Book" button
-const addBookButton = document.getElementById("addBookButton");
-addBookButton.addEventListener("click", addBookToStore);
-
-// Display initial books
+// Display the initial list of books when the page loads
 displayBooks();
